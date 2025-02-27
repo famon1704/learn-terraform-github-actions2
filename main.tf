a203073a9c797ae5dfe93d5b0bf49ed2eb7a5aaf
nama <fairuzu@Kiritos-MacBook-Pro.local>
2025-02-27 12:32:47 +0700

    Final Stage-2

--- main.tf
+++ main.tf
@@ -53,6 +53,7 @@ resource "aws_instance" "web" {
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
+              apt-get install ec2-instance-connect
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
               echo "Hello World" > /var/www/html/index.html
